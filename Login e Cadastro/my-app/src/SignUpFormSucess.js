a168b8e6272d98de338f33b0b66f89dee2d5d622
beatrizvsena <beatriz.sena@bandtec.com.br>
2022-10-23 14:30:49 -0300

    tela cadastro

---
+++ Login e Cadastro/my-app/src/SignUpFormSucess.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+const SignUpFormSucess = () => {
+    return (
+        <div>
+            <div className="container">
+                <div className="app-wrapper">
+                    <h1 className="form-sucess">Conta criada com sucesso!</h1>
+                </div>
+            </div>
+        </div>
+
+        
+    )
+}
+
+export default SignUpFormSucess;

a99b783314d4b7f710f3ff342c02b4944b2eefe0
GabrielStosVitorino <gabrielstosvitorino@gmail.com>
2022-11-27 13:58:13 -0300

    select-avaliacao

--- Site/src/componets/Search/Search.jsx
+++ Site/src/componets/Search/Search.jsx
@@ -1,35 +1,11 @@
 import React from 'react'
 import './Search.css'
 import Lupa from '../../assets/search.svg'
+import Star from '../../assets/star-icon.png'
 import Mulher from '../../assets/woman2.2.jpg'
 import Star5 from '../../assets/star5.png'
 import { LikeButton } from './HeartLike'
 import { Footer } from '../Footer/Footer'
-import api from '../../axios.js';
-
-function pesquisarPorServico(){
-    var tipoServico = 3;
-    var tiposServicos = [];
-
-    if(tipoServico === 1){
-        tiposServicos = [1,4,5,7];
-    } else if (tipoServico === 2){
-        tiposServicos = [2,4,6,7];
-    } else if(tipoServico === 3){
-        tiposServicos = [3,5,6,7];
-    }
-
-    api
-        .get(`/prestadores/filtro-por-servico/${tiposServicos[0]}/${tiposServicos[1]}/${tiposServicos[2]}/${tiposServicos[3]}`)
-        .then((res) => {
-          console.log(res);
-          var prestadores = res.data;
-          console.log(prestadores[0]);
-        }).catch((err) => {
-          console.log(err);
-        })
-
-}
 
 export const Search = () => {
     return (
@@ -39,14 +15,27 @@ export const Search = () => {
                     <div className="find">
                         Encontre um prestador perto de você
                     </div>
+                    <div className="buscas">
                     <div className="select">
                         <img src={Lupa} alt="" />
-                        <select name="" id="" onChange={pesquisarPorServico}>
+                        <select name="" id="">
                             <option selected disabled value="">Escolha um serviço</option>
                             <option value="">Eletétrica</option>
                             <option value="">Hidráulica</option>
                             <option value="">Pintura</option>
                         </select>
+                    </div>
+                    <div className="select sele">
+                        <img src={Star} alt="" />
+                        <select name="" id="">
+                            <option selected disabled value="">Mais avaliados</option>
+                            <option value="">5 estrelas</option>
+                            <option value="">4 estrelas</option>
+                            <option value="">3 estrelas</option>
+                            <option value="">2 estrelas</option>
+                            <option value="">1 estrelas</option>
+                        </select>
+                    </div>
                     </div>
                 </div>
                 <div className="providerss">

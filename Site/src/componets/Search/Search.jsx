825a132dbda57c0ba15cdc75e743f96899817500
GabrielStosVitorino <gabrielstosvitorino@gmail.com>
2022-11-28 17:49:45 -0300

    switch

--- Site/src/componets/Search/Search.jsx
+++ Site/src/componets/Search/Search.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import './Search.css'
 import Lupa from '../../assets/search.svg'
-import Star from '../../assets/star-icon.png'
-import Mulher from '../../assets/woman2.2.jpg'
-import Homem from '../../assets/man2.jpg'
 import { Footer } from '../Footer/Footer'
 import api from '../../axios.js';
 import { ItemPesquisa } from '../ItemPesquisa/ItemPesquisa'
@@ -27,100 +24,106 @@ import { ItemPesquisa } from '../ItemPesquisa/ItemPesquisa'
 // }
 
 
-function pesquisarPorServico(){
+function pesquisarPorServico() {
     var tipoServico = 3;
     var tiposServicos = [];
     var melhores = false;
 
-    if(tipoServico === 1){
-        tiposServicos = [1,4,5,7];
-    } else if (tipoServico === 2){
-        tiposServicos = [2,4,6,7];
-    } else if(tipoServico === 3){
-        tiposServicos = [3,5,6,7];
+    if (tipoServico === 1) {
+        tiposServicos = [1, 4, 5, 7];
+    } else if (tipoServico === 2) {
+        tiposServicos = [2, 4, 6, 7];
+    } else if (tipoServico === 3) {
+        tiposServicos = [3, 5, 6, 7];
     }
 
-    if(melhores === false){
+    if (melhores === false) {
         api
-        .get(`/prestadores/filtro-por-servico/${tiposServicos[0]}/${tiposServicos[1]}/${tiposServicos[2]}/${tiposServicos[3]}`)
-        .then((res) => {
-          console.log(res);
-          var prestadores = res.data;
-          console.log(
-          prestadores.map(prestador => (
-            <ItemPesquisa
-                nome={prestador.nome}
-                imagem={prestador.urlFoto}
-                estrela={prestador.media}
-            />
-            )))
-        }).catch((err) => {
-          console.log(err);
-        })
+            .get(`/prestadores/filtro-por-servico/${tiposServicos[0]}/${tiposServicos[1]}/${tiposServicos[2]}/${tiposServicos[3]}`)
+            .then((res) => {
+                console.log(res);
+                var prestadores = res.data;
+                console.log(
+                    prestadores.map(prestador => (
+                        <ItemPesquisa
+                            nome={prestador.nome}
+                            imagem={prestador.urlFoto}
+                            estrela={prestador.media}
+                        />
+                    )))
+            }).catch((err) => {
+                console.log(err);
+            })
     } else {
         api
-        .get(`/avaliacoes-prestadores/melhores-por-servico/${tiposServicos[0]}/${tiposServicos[1]}/${tiposServicos[2]}/${tiposServicos[3]}`)
-        .then((res) => {
-          console.log(res);
-          var prestadores = res.data;
-          {prestadores.map(prestador => (
-            <ItemPesquisa
-                nome={prestador.nome}
-                imagem={prestador.urlFoto}
-                estrela={prestador.media}
-            />
-            ))}
-        }).catch((err) => {
-          console.log(err);
-        })
+            .get(`/avaliacoes-prestadores/melhores-por-servico/${tiposServicos[0]}/${tiposServicos[1]}/${tiposServicos[2]}/${tiposServicos[3]}`)
+            .then((res) => {
+                console.log(res);
+                var prestadores = res.data;
+                {
+                    prestadores.map(prestador => (
+                        <ItemPesquisa
+                            nome={prestador.nome}
+                            imagem={prestador.urlFoto}
+                            estrela={prestador.media}
+                        />
+                    ))
+                }
+            }).catch((err) => {
+                console.log(err);
+            })
     }
-    
+
 }
 
-function pesquisarPorAvaliacao(){
+function pesquisarPorAvaliacao() {
     var tipoServico = null;
     var tiposServicos = [];
 
-    if(tipoServico == null){
+    if (tipoServico == null) {
         api
-        .get(`/avaliacoes-prestadores/melhores`)
-        .then((res) => {
-          console.log(res);
-          var prestadores = res.data;
-          {prestadores.map(prestador => (
-            <ItemPesquisa
-                nome={prestador.nome}
-                imagem={prestador.urlFoto}
-                estrela={prestador.media}
-            />
-            ))}
-        }).catch((err) => {
-          console.log(err);
-        })
+            .get(`/avaliacoes-prestadores/melhores`)
+            .then((res) => {
+                console.log(res);
+                var prestadores = res.data;
+                {
+                    prestadores.map(prestador => (
+                        <ItemPesquisa
+                            nome={prestador.nome}
+                            imagem={prestador.urlFoto}
+                            estrela={prestador.media}
+                        />
+                    ))
+                }
+            }).catch((err) => {
+                console.log(err);
+            })
     } else {
-        if(tipoServico === 1){
-            tiposServicos = [1,4,5,7];
-        } else if (tipoServico === 2){
-            tiposServicos = [2,4,6,7];
-        } else if(tipoServico === 3){
-            tiposServicos = [3,5,6,7];
+        if (tipoServico === 1) {
+            tiposServicos = [1, 4, 5, 7];
+        } else if (tipoServico === 2) {
+            tiposServicos = [2, 4, 6, 7];
+        } else if (tipoServico === 3) {
+            tiposServicos = [3, 5, 6, 7];
         }
 
         api
-        .get(`/avaliacoes-prestadores/melhores-por-servico/${tiposServicos[0]}/${tiposServicos[1]}/${tiposServicos[2]}/${tiposServicos[3]}`)
-        .then((res) => {
-          console.log(res);
-          var prestadores = res.data;
-          {prestadores.map(prestador => (
-            <ItemPesquisa
-                nome={prestador.nome}
-                imagem={prestador.urlFoto}
-                estrela={prestador.media}
-            />
-            ))}
-        }).catch((err) => {
-          console.log(err);
-        })
+            .get(`/avaliacoes-prestadores/melhores-por-servico/${tiposServicos[0]}/${tiposServicos[1]}/${tiposServicos[2]}/${tiposServicos[3]}`)
+            .then((res) => {
+                console.log(res);
+                var prestadores = res.data;
+                {
+                    prestadores.map(prestador => (
+                        <ItemPesquisa
+                            nome={prestador.nome}
+                            imagem={prestador.urlFoto}
+                            estrela={prestador.media}
+                        />
+                    ))
+                }
+            }).catch((err) => {
+                console.log(err);
+            })
     }
 }
 
@@ -143,22 +146,20 @@ export const Search = () => {
                                 <option value="3">Elétrica</option>
                             </select>
                         </div>
-                        <div className="select sele">
-                            <img src={Star} alt="" />
-                            <select name="" id="">
-                                <option selected disabled value="">Mais avaliados</option>
-                                <option value="">5 estrelas</option>
-                                <option value="">4 estrelas</option>
-                                <option value="">3 estrelas</option>
-                                <option value="">2 estrelas</option>
-                                <option value="">1 estrelas</option>
-                            </select>
+                        <div className="sele">
+                            <label htmlFor='avaliados' className="mais">
+                                <div className="switch">
+                                    <input type="checkbox" name='avaliados' id='avaliados' className='avaliados' />
+                                    <span className="switchButton"></span>
+                                </div>
+                                <span className="melhores">Mais avaliados</span>
+                            </label>
                         </div>
                     </div>
                 </div>
 
                 <div className="cradsPrestador">
-                    
+
                 </div>
             </div>
             <Footer />

683838975797a5d2e9536a32a129bf82d2403a8e
Gabriel Vitorino <gabrielstosvitorino@gmail.com>
2022-12-02 12:26:46 -0300

    validações planos

--- Site/src/componets/PlansInfoPro/PlansInfoPro.jsx
+++ Site/src/componets/PlansInfoPro/PlansInfoPro.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useForm } from 'react'
 import './PlansInfoPro.css'
 import SetaPlan from '../../assets/setaPlans.png'
 import Logo from '../../assets/Logo Construção E Reparos (2).png'
@@ -6,61 +6,97 @@ import Fatura from '../../assets/fatura.png'
 import ReactInputMask from 'react-input-mask'
 import { useNavigate } from 'react-router-dom'
 import Boleto from '../../assets/boletoPremium.pdf'
+import { Formik, Form } from 'formik';
+import { PlansValidate } from './PlansValidate'
+import * as Yup from 'yup';
 
 export const PlansInfoPro = () => {
+
+  const validate = Yup.object({
+    nameCard: Yup.string()
+      .required('Necessário nome do cartão'),
+    numeroCard: Yup.string()
+      .max(16, 'São necessários 16 caracteres')
+      .required('Necessário número do cartão'),
+    email: Yup.string()
+      .email('Email inválido')
+      .required('Email é necessário'),
+    cvc: Yup.string()
+      .max(3, 'São necessários 3 caracteres')
+      .required('CVC necessário'),
+    dataCard: Yup.string()
+      .max(5, 'São necessários 5 caracteres')
+      .required('Data necessária'),
+  })
 
   const navigate = useNavigate();
 
   return (
-    <div className='plansInfoAll'>
-      <div className="plansInfo">
-        <div className="product">
-          <div className="backPlan">
-            <img src={SetaPlan} alt="" onClick={() => navigate("/home")} />
-            <img src={Logo} alt="" />
-          </div>
-          <div className="informationPlan">
-            <span className="nameProdutuc">
-              Plano BASIC
-            </span>
-            <span className="sale">
-              R$109,99
-            </span>
-          </div>
-          <div className="power">
-            <span className="powerBy">Power by </span>
-            <span className="by">ManoTensao</span>
-          </div>
-        </div>
-        <div className="payments">
-          <div className="boleto">
-            <a href={Boleto} download className='bolDown'>
-              <div className="bolButton">
-                <img src={Fatura} alt="" />
-                <span className="boletoButton">Boleto</span>
+    <Formik
+      initialValues={{
+        nameCard: '',
+        numeroCard: '',
+        email: '',
+        cvc: '',
+        dataCard: ''
+      }}
+      validationSchema={validate}
+      onSubmit={values => {
+        console.log(values)
+      }}
+    >
+      {formik => (
+        <div className='plansInfoAll'>
+          <div className="plansInfo">
+            <div className="product">
+              <div className="backPlan">
+                <img src={SetaPlan} alt="" onClick={() => navigate("/home")} />
+                <img src={Logo} alt="" />
               </div>
-            </a>
-          </div>
-          <span className="or">──────────────── Ou pague com o cartão ────────────────</span>
-          <div className="cardEmail">
-            <span className="payEmail">E-mail</span>
-            <input type="text" />
-          </div>
-          <div className="informationCard">
-            <span className="payCard">Informações do cartão</span>
-            <ReactInputMask type="text" className="numberCard" placeholder='1234 1234 1234 1234' mask={"9999 9999 9999 9999"} />
-            <div className="inputCard">
-              <input type="text" className="dateCard" placeholder='MM/YY' maxLength={5} />
-              <input type="text" className="cvcCard" placeholder='CVC' maxLength={3} pattern="([0-9]{3}" />
+              <div className="informationPlan">
+                <span className="nameProdutuc">
+                  Plano BASIC
+                </span>
+                <span className="sale">
+                  R$109,99
+                </span>
+              </div>
+              <div className="power">
+                <span className="powerBy">Power by </span>
+                <span className="by">ManoTensao</span>
+              </div>
+            </div>
+            <div className="payments">
+              <div className="boleto">
+                <a href={Boleto} download className='bolDown'>
+                  <div className="bolButton">
+                    <img src={Fatura} alt="" />
+                    <span className="boletoButton">Boleto</span>
+                  </div>
+                </a>
+              </div>
+              <span className="or">──────────────── Ou pague com o cartão ────────────────</span>
+              <div className="cardEmail">
+                <span className="payEmail">E-mail</span>
+                <PlansValidate type="email" name="email" />
+              </div>
+              <div className="informationCard">
+                <span className="payCard">Informações do cartão</span>
+                <PlansValidate type="text" className="numberCard" placeholder='1234 1234 1234 1234' mask={"9999 9999 9999 9999"} name="numeroCard" />
+                <div className="inputCard">
+                  <PlansValidate type="text" className="dateCard" placeholder='MM/YY' maxLength={5} name="dataCard" />
+                  <PlansValidate type="text" className="cvcCard" placeholder='CVC' maxLength={3} pattern="([0-9]{3}" name="cvc" />
+                </div>
+              </div>
+              <div className="cardName">
+                <span className="nameCard">Nome do cartão</span>
+                <PlansValidate type="text" name="nameCard" />
+              </div>
+              <button type='submit' className="cardButton" onClick={() => navigate("/sucess")} id="btnPro">Pagar</button>
             </div>
           </div>
-          <div className="cardName">
-            <span className="nameCard">Nome do cartão</span>
-            <input type="text" />
-          </div>
-          <button className="cardButton" onClick={() => navigate("/sucess")} id="btnPro">Pagar</button>
         </div>
-      </div>
-    </div>
+      )}
+    </Formik>
   )
 }

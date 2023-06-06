63860fe7d98739899e3a261776b8f6d9537c220b
Gabriel De Lima Da Silva <gabriel.delima@linx.com.br>
2023-06-05 22:13:39 -0300

    Adicionando meta Content-Security-Policy

--- Site/src/App.js
+++ Site/src/App.js
@@ -4,6 +4,7 @@ import { Rota } from './routes';
 function App() {
   return (
     <div className="App">
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
       <Rota />
     </div>
   );

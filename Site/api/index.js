5099d9a0a8effd43d012256ee284a3baa3d6b932
Gabriel De Lima Da Silva <gabriel.delima@linx.com.br>
2023-06-05 22:08:19 -0300

    Adicionando proxy middleware

--- Site/api/index.js
+++ Site/api/index.js
@@ -2,12 +2,19 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const stripe = require("./routes/stripe");
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const products = require("./products");
 
 const app = express();
 
 require("dotenv").config();
+
+app.use('/socket.io', createProxyMiddleware({
+  target: 'https://lbmanotensao.hopto.org:3001',
+  changeOrigin: true,
+  secure: false,
+}));
 
 app.use(express.json());
 app.use(cors());

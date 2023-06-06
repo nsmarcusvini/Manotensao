25496f573859f12eaff486f14c0273643e905655
Gabriel De Lima Da Silva <gabriel.delima@linx.com.br>
2023-06-05 22:18:43 -0300

    Mudando tag meta de lugar

--- Site/src/App.js
+++ Site/src/App.js
@@ -4,7 +4,6 @@ import { Rota } from './routes';
 function App() {
   return (
     <div className="App">
-      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
       <Rota />
     </div>
   );
